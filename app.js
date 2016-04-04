c7038acaf36b0bb799484ddc4bc8196e1429caf9
Felipe Bobadilla <fabobadi@uc.cl>
2016-04-03 21:07:33 -0300

    versio 1

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express')
 const morgan = require('morgan')
-
 const app = express()
+var comments = [];
+var last;
 app.use(morgan('tiny'))
-
-app.get('/', (req, res) => {
-  // Send JSON
-  res.send({ status: 'on' })
+app.set("view engine","jade");
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.get("/", (req, res) => {
+  res.render("index");
 })
-
+app.post("/savedata",function(req, res){
+  last =req.body.data;
+  res.render("save", {data: last});
+  comments.push(last);
+})
+app.get("/showdata", (req, res) => {
+  res.render("data",{data: comments});
+})
 module.exports = app
